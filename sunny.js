4ca2a70382493c63c20f4a2ab27dfeb3367b81e4
demy <93206611+Demyttenaere@users.noreply.github.com>
2022-09-14 11:46:06 +0200

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('btn');
+const menu = document.querySelector('.menu');
+
+btn.addEventListener("click", () => {         
+
+    if (menu.classList.contains('hidden')){
+    menu.classList.remove('hidden');
+} else  {
+    menu.classList.add('hidden');
+}
+          
+})
